0f0e8aafb50efd7d0e28acec026dddd731262068
Tyler Gennaro <tylergennaro10@gmail.com>
2024-03-21 19:01:42 -0400

    added scroll listener to header, changed to sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,36 @@
+'use client';
+
 import { ThemeToggle } from './ThemeToggle';
 import logo from '../../public/logo.svg';
 import Image from 'next/image';
 import { Github, Linkedin } from 'lucide-react';
 import { Button } from './Button';
+import { useEffect, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 export default function Header() {
+	const [isScrolled, setIsScrolled] = useState(false);
+	useEffect(() => {
+		setIsScrolled(window !== undefined ? window.scrollY > 1 : false);
+		const handleScroll = () => {
+			setIsScrolled(window.scrollY > 1);
+		};
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
 	return (
-		<header>
+		<header
+			className={twMerge(
+				'sticky top-0 z-10 bg-background transition',
+				isScrolled && 'border-b shadow-md'
+			)}
+		>
 			<div className='max-w-screen-xl mx-auto px-8 flex justify-between py-4'>
 				<Image
 					src={logo}
 					alt='Logo'
-					className='w-12 aspect-square shadow-lg rounded-full'
+					className='w-10 aspect-square shadow-lg rounded-full'
 				/>
 				<div className='flex gap-2 items-center'>
 					<a href='https://github.com/TGennnaro' target='_blank'>
