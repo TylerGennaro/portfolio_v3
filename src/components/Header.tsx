ee22fa9d666024fbade06229b92b2e761a983cf6
Tyler Gennaro <tylergennaro10@gmail.com>
2024-03-25 08:45:04 -0400

    added email button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@
 import { ThemeToggle } from './ThemeToggle';
 import logo from '../../public/logo.svg';
 import Image from 'next/image';
-import { Github, Linkedin } from 'lucide-react';
+import { Github, Linkedin, Mail } from 'lucide-react';
 import { Button } from './Button';
 import { useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
@@ -33,14 +33,19 @@ export default function Header() {
 					className='w-10 aspect-square shadow-lg rounded-full'
 				/>
 				<div className='flex gap-2 items-center'>
+					<a href='https://www.linkedin.com/in/tyler-gennaro/' target='_blank'>
+						<Button variant='icon'>
+							<Linkedin />
+						</Button>
+					</a>
 					<a href='https://github.com/TGennnaro' target='_blank'>
 						<Button variant='icon'>
 							<Github />
 						</Button>
 					</a>
-					<a href='https://www.linkedin.com/in/tyler-gennaro/' target='_blank'>
+					<a href='mailto:tylergennaro10@gmail.com'>
 						<Button variant='icon'>
-							<Linkedin />
+							<Mail />
 						</Button>
 					</a>
 					<ThemeToggle />
