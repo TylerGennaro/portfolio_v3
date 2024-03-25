007b0ff447c120787e2f20e91a98667d4166198b
Tyler Gennaro <tylergennaro10@gmail.com>
2024-03-25 08:36:32 -0400

    added muted btn variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,15 +4,15 @@ import { twMerge } from 'tailwind-merge';
 import { VariantProps, tv } from 'tailwind-variants';
 
 const button = tv({
-	base: 'py-2 px-8 rounded-lg shadow-md text-foreground',
+	base: 'py-2 px-8 rounded-lg shadow-md text-foreground transition duration-300',
 	variants: {
 		variant: {
-			primary:
-				'bg-primary text-white hover:brightness-125 transition duration-300',
-			outline: 'bg-background border hover:bg-content transition duration-300',
+			primary: 'bg-primary text-white hover:brightness-125',
+			outline: 'bg-background border hover:bg-content',
+			muted: 'bg-content hover:brightness-90',
 			light:
-				'bg-transparent border border-white text-white hover:bg-white hover:text-primary transition duration-300',
-			icon: 'bg-transparent border hover:bg-content transition duration-300 p-2 shadow-none',
+				'bg-transparent border border-white text-white hover:bg-white hover:text-primary',
+			icon: 'bg-transparent border hover:bg-content p-2 shadow-none',
 		},
 	},
 	defaultVariants: {
